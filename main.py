775d09e49ddf6b11e08b3b0a62e019b05ab21396
jsrimr <jsrimr@naver.com>
2020-04-27 23:25:50 +0900

    send data

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import requests
 
 if __name__ == '__main__':
     event = "github_prac"
     target_url = f'https://maker.ifttt.com/trigger/{event}/with/key/do9TqGuHyqJyXbff2ufCjc'
-    requests.post(target_url, data = { "value1" : "1", "value2" : "2", "value3" : "3" })
+    requests.post(target_url, data = { "value1" : os.listdir()})
 
