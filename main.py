0db01862255254f6ed8add72edfb0df7d4db043b
jsrimr <jsrimr@naver.com>
2020-04-28 00:12:51 +0900

    - requirement 통과하는지 - 외부데이터읽는거 가능한지

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 import requests
+import pandas as pd
+
+data = pd.read_csv("bitcoin_data.csv")
+
 
 if __name__ == '__main__':
     event = "github_prac"
     target_url = f'https://maker.ifttt.com/trigger/{event}/with/key/do9TqGuHyqJyXbff2ufCjc'
-    requests.post(target_url, data = { "value1" : os.listdir()})
+    requests.post(target_url, data = { "value1" : os.listdir(), "value2":data['c'].head(1).values[0])
 
